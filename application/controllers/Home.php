05c5d5a3918512f5a0ef05330918b68005c14fbd
codexsleep <firmanraiwan@gmail.com>
2022-07-02 14:31:54 +0700

    sikos home frontend

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['judul'] = "Home";
+        $this->load->view('frontend/layout/header', $data);
+        $this->load->view("frontend/vw_home", $data);
+        $this->load->view('frontend/layout/footer', $data);
+    }
+
+}
